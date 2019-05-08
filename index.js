abf68b7fc64a667e6d9009f5adccd1db4cba14be
Renaldy Pratama <renaldy.pratama19@gmail.com>
2019-05-08 14:49:30 +0700

    finish day 2 video

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 import express from 'express';
-import {
-  getInstagramCount,
-  getTwitterCount
-} from './lib/scraper';
+import { getInstagramCount, getTwitterCount } from './lib/scraper';
+import './lib/cron';
 
 const app = express();
 
 app.get('/scrape', async (req, res, next) => {
-  console.log('Scraping!');
+  console.log('Scraping!!');
   const [iCount, tCount] = await Promise.all([
     getInstagramCount(),
     getTwitterCount()
   ]);
-  console.log(iCount, tCount);
   res.json({iCount, tCount});
 });
 
-app.listen(2093, deets =>
-  console.log(deets)
-  // console.log(`Example App running on port ${deets.PORT}`)
-);
+app.listen(2093, () => {
+  console.log(`Example App running on port 2093`);
+});
