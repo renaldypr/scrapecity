e4047fbc39ddf0d25aceec5d82a3c099e79a902b
Renaldy Pratama <renaldy.pratama19@gmail.com>
2019-05-09 17:45:25 +0700

    finish day 5 video

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import { getInstagramCount, getTwitterCount } from './lib/scraper';
 import db from './lib/db';
 import { uniqueCount } from './lib/utils';
 import './lib/cron';
+import aggregate from './lib/aggregate';
 
 const app = express();
 app.use(cors());
@@ -17,14 +18,15 @@ app.get('/scrape', async (req, res, next) => {
   res.json({iCount, tCount});
 });
 
-app.get('/data', async (req, res, next) => {
+app.get('/aggregate', async (req, res, next) => {
   // get the scrape data
-  const {twitter, instagram} = db.value();
+  const { twitter, instagram } = db.value();
   // filter for only unique values
   const uniqueTwitter = uniqueCount(twitter);
   const uniqueInstagram = uniqueCount(instagram);
+  // need to aggregate thiese values
   // respond with json
-  res.json({ twitter: uniqueTwitter, instagram: uniqueInstagram });
+  res.json({ twitter: aggregate(twitter), instagram: aggregate(instagram) });
 })
 
 app.listen(2093, () => {
